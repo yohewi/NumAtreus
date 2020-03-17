27085b425ec801f560510acaf783f466af67c123
yohewi <yohewi@gmail.com>
2020-03-18 08:14:15 +0900

    20200318changed

--- keymap.c
+++ keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SLSH ,
     SFT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_UP,  SFT_T(KC_DOT) ,
-    KC_LALT, KC_TAB, KC_LCTL,  MO(_LOWER), KC_SPC, KC_BSPC, LALT(KC_GRV), KC_ENT,  MO(_RAISE),  KC_LEFT, KC_DOWN, KC_RIGH ),
+    KC_LALT, KC_TAB, KC_LCTL,  MO(_LOWER), KC_SPC, KC_BSPC, LALT(KC_GRV), KC_ENT,  MO(_RAISE),  KC_LEFT, KC_DOWN, KC_RGHT ),
 
 
   /*
